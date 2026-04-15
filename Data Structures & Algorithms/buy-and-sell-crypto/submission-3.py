d07af7aabeae344b1e262e2bccff7e53b23d1a95
Talha Tariq <talhat@umd.edu>
2026-04-15 13:50:40 -0400

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min = prices[0]
+        diff = 0
+        for i, price in enumerate(prices):
+            if price < min:
+                min = price
+            if price - min > diff:
+                diff = price - min
+        
+        return diff
