392815b9e351ce8978488cf40930f5becfc04d30
Talha Tariq <talhat@umd.edu>
2026-04-27 20:43:33 -0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        temp = [[position[i],speed[i]] for i in range(len(position))]
+        temp.sort(reverse = True)
+        stack = []
+
+        print(temp)
+        
+        for i in temp:
+            dist = target - i[0]
+            time = dist/i[1]
+            
+            if stack and time > stack[-1]:
+                stack.append(time)
+            elif not stack:
+                stack.append(time)
+
+        return len(stack)
